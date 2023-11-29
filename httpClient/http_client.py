3db09c79f42368d404039dc037ea579fec7e82b3
stane000 <80036090+stane000@users.noreply.github.com>
2023-11-29 15:03:01 +0100

    Add files via upload

---
+++ httpClient/http_client.py
@@ -0,0 +1,83 @@
+
+
+from dataclasses import dataclass, asdict
+from typing import List, Dict
+
+from requests import Session
+
+# from .models import Company, Job, Worker
+from .models import Company, Job, Worker, Position
+
+@dataclass
+class ResponseList:
+
+    response: List[Dict]
+    status_code: int
+    
+    @classmethod
+    def return_respone(cls, response):
+        if response.status_code == 200:
+            return  cls(response.json(), response.status_code)
+        else: 
+            return cls([], response.status_code)
+        
+@dataclass
+class ResponseBool:
+
+    response: bool
+    status_code: int
+
+
+class FindJobClient:
+
+    def __init__(self) -> None:
+        self.local_jost = "https://localhost:7044"
+        self.session = Session()
+
+    def get_companies(self) -> ResponseList:
+        response = self.session.get(f'{self.local_jost}/api/Company', verify=False)
+        return ResponseList.return_respone(response)
+    
+    def get_workers(self) -> ResponseList:
+        response = self.session.get(f'{self.local_jost}/api/Worker', verify=False)
+        return ResponseList.return_respone(response)
+    
+    def get_jobs(self) -> ResponseList:
+        response = self.session.get(f'{self.local_jost}/api/Job', verify=False)
+        return ResponseList.return_respone(response)
+    
+    def post_compnay(self, compnay: Company):
+        response = self.session.post(f'{self.local_jost}/api/Company', json=asdict(compnay), 
+                                     headers={'accept': '*/*', 'Content-Type': 'application/json'}, verify=False)
+        return ResponseBool(response.ok, response.status_code )
+
+    def post_job(self, job: Job):
+        response = self.session.post(f'{self.local_jost}/api/Job', json=asdict(job), 
+                                     headers={'accept': '*/*', 'Content-Type': 'application/json'}, verify=False)
+        return ResponseBool(response.ok, response.status_code )   
+    
+    def post_worker(self, worker: Worker):
+        response = self.session.post(f'{self.local_jost}/api/Worker', json=asdict(worker), 
+                                     headers={'accept': '*/*', 'Content-Type': 'application/json'}, verify=False)
+        return ResponseBool(response.ok, response.status_code )   
+    
+    # def post_worker(self, worker: Worker):
+    #     response = self.session.post(f'{self.local_jost}/api/Worker', json=asdict(worker), 
+    #                                  headers={'accept': '*/*', 'Content-Type': 'application/json'}, verify=False)
+    #     return ResponseBool(response.ok, response.status_code )
+    
+        
+if __name__ == "__main__":
+
+    client = FindJobClient()
+    companies = companies = client.get_companies()
+    a  = 5
+    c = Company(**companies.response[0])
+    # company = client.post_compnay(Company.create("Rimac3", 5000, [], False, []))
+    # a = 5
+    # job = Job(companies, Position.BackEndDeveloper, 3, Collage.ALGEBRA)
+    worker = Worker.create(firstName="Hrvoje", lastName="Nezic", age=60, college="FER", position=f"{Position.BackEndDeveloper.name}", experience=30,
+                            lookingForJob=False, companyName=c.name)
+    a = client.post_worker(worker)
+    b = 3
+    # v = client.post_job(job)
---
+++ httpClient/models.py
@@ -0,0 +1,123 @@
+
+
+from dataclasses import dataclass
+from enum import Enum
+import random   
+from faker import Faker
+from typing import List, Optional
+
+
+class Position(Enum):
+    SoftwareEngineer = 0,
+    SoftwareTestEngineer = 1,
+    FrontEndDeveloper = 2,
+    BackEndDeveloper = 3,
+    FullStackDeveloper = 4,
+
+    JuniorSoftwareEngineer = 5,
+    JuniorSoftwareTestEngineer  = 6,
+    JuniorFrontEndDeveloper = 7, 
+    JuniorBackEndDeveloper = 8, 
+    JuniorFullStackDeveloper = 9,
+
+    SeniorSoftwareEngineer = 10,
+    SeniorSoftwareTestEngineer = 11,
+    SeniorFrontEndDeveloper = 12,
+    SeniorBackEndDeveloper = 13,
+    SeniorFullStackDeveloper = 14,
+
+    ProjectManager = 15
+
+class Collage(Enum):
+    TVZ = 0,
+    FER = 1,
+    PMF = 2,
+    ALGEBRA = 3,
+    FESB = 4
+
+@dataclass
+class Company:
+
+    id: int
+    name: str
+    avgSalary: int
+    workers: List
+    hiring: bool
+    jobs: []
+
+    @classmethod
+    def create(cls, name, avgSalary, workers, hiring, jobs):   
+        return cls(0, name, avgSalary, workers, hiring, jobs) 
+    
+    @staticmethod
+    def create_fake_company():
+        fake = Faker()
+        # Generate a fake Company instance
+        return Company.create(
+                name=fake.company(),
+                avgSalary=fake.random_int(min=0, max=1000),
+                workers=[],
+                hiring=fake.boolean(),
+                jobs=[]
+            )
+
+@dataclass
+class Job:
+    
+    id: int
+    company: Company
+    position: Position
+    experience: int
+    collage: Collage
+
+    @classmethod
+    def create(cls, company, position, experience, collage):   
+        return cls(0, company, position, experience, collage) 
+
+    @classmethod
+    def create_fake_job(cls, company: Company):  
+        fake = Faker()
+        # Generate a fake Job instance
+        return cls(
+                id=0,
+                company=company,
+                position= random.choice(list(Position)),
+                experience=fake.random_int(min=0, max=35),
+                collage= random.choice(list(Position)
+            ))
+    
+@dataclass
+class Worker:
+
+    id: int
+    firstName: str
+    lastName: str
+    age: int
+    college: str
+    position: str
+    experience: int
+    lookingForJob: bool	
+    companyName: str
+
+    @classmethod
+    def create(cls, firstName: str, lastName: str, age: int, college: str, position: str, 
+               experience: int, lookingForJob: bool, companyName: str):
+        return cls(0, firstName, lastName, age, college, position, 
+                   experience, lookingForJob, companyName)  
+    
+    @classmethod
+    def create_fake_worker(cls, company: Company):  
+        fake = Faker()
+        # Generate a fake Job instance
+        return cls(
+                id=0,
+                firstName=fake.first_name(),
+                lastName=fake.last_name(),
+                age=fake.random_int(min=0, max=65),
+                college= random.choice(list(Collage)).name,
+                position= random.choice(list(Position)).name,
+                experience=fake.random_int(min=0, max=35),
+                lookingForJob=fake.boolean(), 
+                companyName=company.name
+            )
+    
